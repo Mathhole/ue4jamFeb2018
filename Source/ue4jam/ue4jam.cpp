39cd2ec1ea1e9e2bfb7caed96fdc4638f73d3d5f
Nathan Rowe <nathan@sculptrvr.com>
2018-02-08 11:20:28 -0800

    Initial Commit of project. Huzzah!

---
+++ Source/ue4jam/ue4jam.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ue4jam.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ue4jam, "ue4jam" );
